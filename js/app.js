c7ac67fa7002b7e8be2eca1b3402d014042479aa
elroy404 <sdzgrail@gmail.com>
2016-10-02 19:48:00 -0700

    10/2

--- js/app.js
+++ js/app.js
@@ -2,29 +2,79 @@ $(document).ready(function(){
 
   var column1, column2, column3 = [];
   var rowA, rowB, rowC = [];
-  var ex = false;
-  var oh = true;
-  var marker = circle;
+  var placesTaken = [];
+  var ex, oh, marker = '';
   var circle = '<i class="fa fa-circle-o" aria-hidden="true"></i>';
   var times = '<i class="fa fa-times" aria-hidden="true"></i>';
 
 
-  $('#game-board').on('click','div',function(){
-    var selectedId = '#' + this.id;
-    console.log(selectedId);
-    $(selectedId).html(circle);
-    $(selectedId).toggle(oh);
-    console.log(oh);
+  //randomly selects whos going to start the game
+  function startGame(){
+    ex, oh, marker = '';
+    var randomNum = Math.floor((Math.random()*2)+1);
+
+    if(randomNum == 1){
+      marker = times;
+      ex = true;
+      oh = false;
+    }
+    else{
+      marker = circle;
+      ex = false;
+      oh = true;
+    }
+    console.log("Who goes first: " + marker);
+  }
+
+  //button clicked to initiate randomize player start
+  $('#new-game').on('click',function(){
+    startGame();
   });
 
+  //game gui
+  $('#game-board').on('click','div',function(){
+    playerTurn();
+    var squareId = this.id;
+
+
+    placesTaken.push(this.id);
+    console.log(placesTaken);
+
+    squareTaken(squareId);
+  });
+
+  //switches which player is up
   function playerTurn(){
       if(oh == true){
         marker = circle;
+        oh = false;
+        ex = true;
       }
       else{
         marker = times;
+        oh = true;
+        ex = false;
       }
   }
+
+  //checks if square was taken
+  function squareTaken(squareId){
+    var selectedId = '#' + squareId;
+    console.log(selectedId);
+
+    for(var i in placesTaken){
+      if(placesTaken[i] === squareId){
+        console.log('square taken');
+
+      }
+      else{
+        console.log("not taken");
+        $(selectedId).html(marker);
+      }
+    }
+  }
+
+
 
 
 });
