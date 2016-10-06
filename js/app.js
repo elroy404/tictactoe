c8b9dbbb8afe19d47d69dd50a6b494ffcf738cd6
elroy404 <sdzgrail@gmail.com>
2016-10-05 20:58:08 -0700

    10/5

--- js/app.js
+++ js/app.js
@@ -1,78 +1,113 @@
 $(document).ready(function(){
 
-  var column1, column2, column3 = [];
-  var rowA, rowB, rowC = [];
-  var placesTaken = [];
-  var ex, oh, marker = '';
+  var placesTakenPosition, placesTakenPlayer = [];
+  var ex, oh, marker, player = '';
   var circle = '<i class="fa fa-circle-o" aria-hidden="true"></i>';
   var times = '<i class="fa fa-times" aria-hidden="true"></i>';
 
-
   //randomly selects whos going to start the game
+  //makes gameboard avaiable for playing
   function startGame(){
     ex, oh, marker = '';
     var randomNum = Math.floor((Math.random()*2)+1);
 
     if(randomNum == 1){
       marker = times;
+      player = "X";
       ex = true;
       oh = false;
     }
     else{
       marker = circle;
+      player = "O";
       ex = false;
       oh = true;
     }
-    console.log("Who goes first: " + marker);
+    $("#whos-turn").html("Player Turn: " + player);
+
   }
 
   //button clicked to initiate randomize player start
   $('#new-game').on('click',function(){
+    newGame();
     startGame();
+    $("#gameboard").show();
+    $("#question").hide();
   });
 
   //game gui
-  $('#game-board').on('click','div',function(){
-    playerTurn();
+  $('#gameboard').on('click','div',function(){
     var squareId = this.id;
-
-
-    placesTaken.push(this.id);
-    console.log(placesTaken);
-
-    squareTaken(squareId);
+    playerTurn(squareId);
+    // squareTaken(squareId);
+    console.log(placesTakenPlayer);
+    console.log(placesTakenPosition);
+    $("#whos-turn").html("Player Turn: " + player);
   });
 
+  //stores marker placement positions
+  function storageContainer(squareId){
+    var squareIdNum = parseInt(squareId);
+    var selectedId = '#' + squareId;
+    $(selectedId).html(marker);
+    placesTakenPlayer.splice(squareIdNum,1,player);
+    placesTakenPosition.push(squareIdNum);
+  }
+
   //switches which player is up
-  function playerTurn(){
+  function playerTurn(squareId){
+      console.log("before change:" + player);
       if(oh == true){
         marker = circle;
+        storageContainer(squareId);
+        //player switch
+        player = "X";
         oh = false;
         ex = true;
       }
       else{
         marker = times;
+        storageContainer(squareId);
+        //player switch
+        player = "O";
         oh = true;
         ex = false;
       }
   }
 
-  //checks if square was taken
-  function squareTaken(squareId){
-    var selectedId = '#' + squareId;
-    console.log(selectedId);
-
-    for(var i in placesTaken){
-      if(placesTaken[i] === squareId){
-        console.log('square taken');
-
-      }
-      else{
-        console.log("not taken");
-        $(selectedId).html(marker);
-      }
+  //clears game board and storage
+  function newGame(){
+    for(i in placesTakenPosition){
+      // console.log(placesTakenPosition[i]);
+      var foo = "#" + placesTakenPosition[i];
+      $(foo).html("")
     }
+    placesTakenPosition = [];
+    placesTakenPlayer = ["","","","","","","","",""];
+    ex, oh, marker, player = '';
   }
+
+  //decides who won the game
+  function chickenDinner(){
+
+  }
+
+  //checks if square was taken
+  // function squareTaken(squareId){
+  //   var selectedId = '#' + squareId;
+  //   console.log(selectedId);
+  //
+  //   for(var i in placesTakenPosition){
+  //     if(placesTakenPosition[i] != squareId){
+  //       console.log('square taken');
+  //     }
+  //     else{
+  //       placesTakenPosition.push(this.id);
+  //       console.log("not taken");
+  //       $(selectedId).html(marker);
+  //     }
+  //   }
+  // }
 
 
 
